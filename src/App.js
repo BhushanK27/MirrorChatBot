6b6661df69e80d4f367b113c3f5a93c4276769b1
Bhushan Kulkarni <bkkulkarni39@gmail.com>
2022-03-12 14:26:54 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import './App.css';
+import logo from "./images/chatbot.jpg"
+import React, { useState,useEffect,useRef } from 'react'
+
+function App() {
+  const [usermsg, setuserMsg] = useState([]);
+  const [msg, setMsg] = useState("");
+  const displayref = useRef(null);
+
+  useEffect(() => {
+    const savedMessages = JSON.parse(localStorage.getItem('user'))
+    if (savedMessages !== null) {
+      setuserMsg(savedMessages)
+    }
+  }, []);
+
+  const scrollToBottom = () => {
+    displayref.current.scrollIntoView({ behavior: "smooth", block: "end"});
+  };
+  useEffect(scrollToBottom, [msg]);
+
+  const handleKeyPress=(event)=> {
+    if(event.key==='Enter' || event.keyCode===13)
+    {
+      // console.log("Enter pressed")
+      displayer();
+    }
+  }
+
+  const displayer = () => {
+    if ((msg !== "" && msg !== " ") || msg.length < 0)
+    { 
+      usermsg.push(msg)
+    }
+      // console.log(usermsg)
+      setMsg("")
+      
+      localStorage.setItem('user', JSON.stringify(usermsg))
+
+  }
+  return (
+    <div className="App">
+      <div className="main">
+
+        <div id="Header">
+          <img src= {logo} alt="chatbot logo" />
+          <p id={"headertxt"}>ChatBot</p>
+        </div>
+
+        <div className='display'  >
+          <div className='ul' ref={displayref}>
+            {
+              usermsg.length > 0 &&
+              usermsg.map((item, i) => (
+                <>
+
+                  <div id={"user"} key={i}>
+                    {item}
+                  </div> <br/>
+                  <div id={'bot'} >{item} </div>
+                  <br />
+
+                </>
+              
+              ))
+            }
+          </div>
+        </div>
+
+      </div>
+      <div id='Footer'>
+        <input id="msg" autoComplete='off' placeholder="Type here..." value={msg} onChange={(e) => setMsg(e.target.value)} onKeyPress={handleKeyPress}></input>{" "}
+        <button 
+        onClick={displayer}>Send</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
